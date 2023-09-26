68070320331bdb24771183849ec0a9f832d59bdd
Shivanktyagi07 <shivanktyagi976@gmail.com>
2023-09-26 12:05:15 +0530

    react-project

---
+++ contextAPIproject/src/context/UserContext.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// const USerContext = React.createContext()
+
+
+// export default USerContext;
+
+import React  from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
