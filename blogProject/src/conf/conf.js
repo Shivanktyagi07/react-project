ff2b60e8bf6cda48a001d5bc6f5b35ed4f22205b
Shivanktyagi07 <shivanktyagi976@gmail.com>
2023-12-26 11:49:05 +0530

    auth setup

--- blogProject/src/conf/conf.js
+++ blogProject/src/conf/conf.js
@@ -1,4 +1,4 @@
-const config = {
+const conf = {
       
     appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId : String(import.meta.env.VITE_PROJECT_ID),
@@ -8,4 +8,4 @@ const config = {
 
 }
 
-export default config
+export default conf
