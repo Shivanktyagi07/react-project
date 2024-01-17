3a1c61a21c73ff8907175029c707662812eaf69a
Shivanktyagi07 <shivanktyagi976@gmail.com>
2024-01-17 18:04:47 +0530

    setup store folder using reduxToolkit

---
+++ blogProject/src/store/authSLice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState ={
+    status: false,
+    userData: null
+}
+
+const authSLice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action)=> {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
+} )
+    
+export const {} = authSLice.actions;
+
+export default authSLice.reducer;
+
