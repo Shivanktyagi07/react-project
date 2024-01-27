a8b5e3a06364a8199f05aac8f6d7416fb75ea62f
Shivanktyagi07 <shivanktyagi976@gmail.com>
2024-01-27 17:05:30 +0530

    login component created

---
+++ blogProject/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+import {Button, Input, Logo} from "./index"
+import {useDispatch} from "react-redux"
+import authService from "../appwrite/auth"
+import {useForm} from "react-hook-form"
+
+
+function Login() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("")
+
+    const login = async(data) => {
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if (session) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(authLogin(userData));
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
